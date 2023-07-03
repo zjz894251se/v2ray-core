13882a6c31c9b5c3a353f2b05569e04d2950daf3
zjz <zjz894251se@gmail.com>
2023-07-03 16:12:36 +0800

    统计v2ray节点连接时长功能

--- custom/ctx.go
+++ custom/ctx.go
@@ -4,37 +4,61 @@ import (
 	"context"
 	"github.com/v2fly/v2ray-core/v5/common/session"
 	"sync"
+	"time"
 )
+
+type clientInfo struct {
+	ClientId     string    // id
+	StartConnect time.Time // 开始链接时间
+}
 
 type SessionClientKV struct {
 	sync.Mutex
-	mps        map[session.ID]string
-	onBindCall func(session.ID, string)
+	mps          map[session.ID]*clientInfo
+	onBindCall   func(string)
+	onUnbindCall func(string, time.Duration)
 }
 
 func (kv *SessionClientKV) Bind(sessionId session.ID, clientId string) {
 	kv.Lock()
 	defer kv.Unlock()
-	kv.mps[sessionId] = clientId
+	kv.mps[sessionId] = &clientInfo{
+		ClientId:     clientId,
+		StartConnect: time.Now(),
+	}
 }
 
 func (kv *SessionClientKV) GetClientId(sessionId session.ID) string {
 	kv.Lock()
 	defer kv.Unlock()
-	return kv.mps[sessionId]
+	info, ok := kv.mps[sessionId]
+	if ok {
+		return info.ClientId
+	}
+	return ""
 }
 
 func (kv *SessionClientKV) UnBind(sessionId session.ID) {
 	kv.Lock()
 	defer kv.Unlock()
-	delete(kv.mps, sessionId)
+	info, ok := kv.mps[sessionId]
+	if ok {
+		delete(kv.mps, sessionId)
+		if kv.onUnbindCall != nil {
+			kv.onUnbindCall(info.ClientId, time.Duration(time.Now().UnixNano()-info.StartConnect.UnixNano()))
+		}
+	}
 }
 
-func (kv *SessionClientKV) RegisterOnBindCall(call func(session.ID, string)) {
+func (kv *SessionClientKV) RegisterOnBindCall(call func(string)) {
 	kv.onBindCall = call
 }
 
-var _sessionClientKv = &SessionClientKV{mps: make(map[session.ID]string)}
+func (kv *SessionClientKV) RegisterOnUnbindCall(call func(string, time.Duration)) {
+	kv.onUnbindCall = call
+}
+
+var _sessionClientKv = &SessionClientKV{mps: make(map[session.ID]*clientInfo)}
 
 func BindSessionClientId(ctx context.Context, clientId string) {
 	_sessionClientKv.Bind(session.IDFromContext(ctx), clientId)
@@ -48,6 +72,12 @@ func UnBindSessionClientId(ctx context.Context) {
 	_sessionClientKv.UnBind(session.IDFromContext(ctx))
 }
 
-func RegisterSessionOnBindCall(call func(session.ID, string)) {
+// RegisterSessionOnBindCall 客户链接回调
+func RegisterSessionOnBindCall(call func(string)) {
 	_sessionClientKv.RegisterOnBindCall(call)
+}
+
+// RegisterSessionOnUnbindCall 客户断开回调
+func RegisterSessionOnUnbindCall(call func(string, time.Duration)) {
+	_sessionClientKv.RegisterOnUnbindCall(call)
 }
